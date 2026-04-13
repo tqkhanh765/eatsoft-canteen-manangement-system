3418f9751003d880dc56f6afe12cc2c88aa64251
Nguyễn Văn Bảo Duy <baoduy.song@gmail.com>
2026-04-13 21:44:38 +0700

    fix: add api connect to order cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import HomePage from './homepage/pages/HomePage';
+import OrderCart from './customer-order-tab/pages/OrderCart';
 import AuthModal from './homepage/components/AuthModal';
 import './App.css';
 
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="app">
       <Navbar onLoginClick={() => openAuth('login')} />
-      <HomePage />
+      <OrderCart />
       <Footer />
       <AuthModal
         isOpen={authOpen}
